9fb66e0d29c155025575ed01ea61e600c5b2f507
SashaDch <52377489+SashaDch@users.noreply.github.com>
2025-03-29 06:13:13 +0300

    Update TaxSystems.java
    {} syntax

--- ru/netology/tax/TaxSystems.java
+++ ru/netology/tax/TaxSystems.java
@@ -6,17 +6,19 @@ public class TaxSystems {
     }
 
     private static int calcTaxEarnSpend(int earnings, int spendings) {
-        if (earnings <= spendings)
+        if (earnings <= spendings) {
             return 0;
+        }
         return (int)Math.ceil((earnings - spendings) * 0.15);
     }
 
     public static void analyse(int earnings, int spendings) {
         int taxEarn = calcTaxEarn(earnings);
         int taxEarnSpend = calcTaxEarnSpend(earnings, spendings);
-        if (taxEarn == taxEarnSpend)
+        if (taxEarn == taxEarnSpend) {
             System.out.println("Можете выбрать любую систему налогообложения\n" +
                     "Ваш налог составит: " + taxEarn + "рублей");
+        }
         else {
             System.out.println("Мы советуем вам УСН " +
                     (taxEarn < taxEarnSpend ? "доходы\n" : "доходы минус расходы\n") +
